8d646f7a91c63b592f1532df5dd95b3f83d6ede7
Maciek <maciekgrzela45@gmail.com>
2020-02-29 13:01:01 +0100

    End of section 4

--- Persistance/DataContext.cs
+++ Persistance/DataContext.cs
@@ -4,18 +4,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Persistance
 {
-    public class DataContext : DbContext {
+    public class DataContext : DbContext
+    {
         public DataContext(DbContextOptions options) : base(options) { }
-
         public DbSet<Value> Values { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder builder){
-            builder.Entity<Value>().HasData(
-                new Value {Id = 1, Name="Value 101"},
-                new Value {Id = 2, Name="Value 102"},
-                new Value {Id = 3, Name="Value 103"},
-                new Value {Id = 4, Name="Value 104"}
-            );
-        }
+        public DbSet<Activity> Activities { get; set; }
     }
 }
