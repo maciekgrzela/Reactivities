3bdffb57eed34d82759b92904a7527641696c74a
Maciek Grzela <maciekgrzela45@gmail.com>
2020-03-02 11:00:57 +0100

    Add appsettings and implement MobX

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,12 +1,21 @@
-import React, { useState, useEffect, Fragment, SyntheticEvent } from 'react';
+import React, {
+  useState,
+  useEffect,
+  Fragment,
+  SyntheticEvent,
+  useContext
+} from 'react';
 import { Container } from 'semantic-ui-react';
 import { IActivity } from '../models/activity';
 import { Navbar } from '../../features/nav/Navbar';
-import { ActivityDashboard } from '../../features/activities/ActivityDashboard';
+import ActivityDashboard from '../../features/activities/ActivityDashboard';
 import agent from '../api/agent';
 import { LoadingComponent } from './LoadingComponent';
+import ActivityStore from '../stores/activityStore';
+import { observer } from 'mobx-react-lite';
 
 const App = () => {
+  const activityStore = useContext(ActivityStore);
   const [activities, setActivities] = useState<IActivity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
     null
@@ -72,26 +81,18 @@ const App = () => {
   };
 
   useEffect(() => {
-    agent.Activities.list()
-      .then(response => {
-        let activities: IActivity[] = [];
-        response.forEach(activity => {
-          activity.date = activity.date.split('.')[0];
-          activities.push(activity);
-        });
-        setActivities(activities);
-      })
-      .then(() => setLoading(false));
-  }, []);
+    activityStore.loadActivities();
+  }, [activityStore]);
 
-  if (loading) return <LoadingComponent content='Loading activities...' />;
+  if (activityStore.loadingInitial)
+    return <LoadingComponent content='Loading activities...' />;
 
   return (
     <Fragment>
       <Navbar openCreateForm={handleOpenCreateForm} />
       <Container style={{ marginTop: '7em' }}>
         <ActivityDashboard
-          activities={activities}
+          activities={activityStore.activities}
           selectActivity={handleSelectActivity}
           selectedActivity={selectedActivity}
           editMode={editMode}
@@ -108,4 +109,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default observer(App);
--- client-app/src/features/activities/ActivityDashboard.tsx
+++ client-app/src/features/activities/ActivityDashboard.tsx
@@ -1,9 +1,10 @@
 import React, { SyntheticEvent } from 'react';
 import { Grid } from 'semantic-ui-react';
 import { IActivity } from '../../app/models/activity';
-import { ActivityList } from './ActivityList';
+import ActivityList from './ActivityList';
 import { ActivityDetails } from './details/ActivityDetails';
 import { ActivityForm } from './form/ActivityForm';
+import { observer } from 'mobx-react-lite';
 
 interface IProps {
   activities: IActivity[];
@@ -22,7 +23,7 @@ interface IProps {
   target: string;
 }
 
-export const ActivityDashboard: React.FC<IProps> = props => {
+const ActivityDashboard: React.FC<IProps> = props => {
   return (
     <Grid>
       <Grid.Column width={10}>
@@ -56,3 +57,5 @@ export const ActivityDashboard: React.FC<IProps> = props => {
     </Grid>
   );
 };
+
+export default observer(ActivityDashboard);
