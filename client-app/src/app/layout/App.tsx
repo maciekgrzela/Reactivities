f3088f72381db875cc1310557cbbadef607e79ed
Maciek Grzela <maciekgrzela45@gmail.com>
2020-03-17 21:21:57 +0100

    App without stores - working

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,21 +1,12 @@
-import React, {
-  useState,
-  useEffect,
-  Fragment,
-  SyntheticEvent,
-  useContext
-} from 'react';
+import React, { useState, useEffect, Fragment, SyntheticEvent } from 'react';
 import { Container } from 'semantic-ui-react';
 import { IActivity } from '../models/activity';
 import { Navbar } from '../../features/nav/Navbar';
 import ActivityDashboard from '../../features/activities/ActivityDashboard';
 import agent from '../api/agent';
 import { LoadingComponent } from './LoadingComponent';
-import ActivityStore from '../stores/activityStore';
-import { observer } from 'mobx-react-lite';
 
 const App = () => {
-  const activityStore = useContext(ActivityStore);
   const [activities, setActivities] = useState<IActivity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
     null
@@ -81,18 +72,26 @@ const App = () => {
   };
 
   useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore]);
+    agent.Activities.list()
+      .then(response => {
+        let activities: IActivity[] = [];
+        response.forEach(activity => {
+          activity.date = activity.date.split('.')[0];
+          activities.push(activity);
+        });
+        setActivities(activities);
+      })
+      .then(() => setLoading(false));
+  }, []);
 
-  if (activityStore.loadingInitial)
-    return <LoadingComponent content='Loading activities...' />;
+  if (loading) return <LoadingComponent content='Loading activities...' />;
 
   return (
     <Fragment>
       <Navbar openCreateForm={handleOpenCreateForm} />
       <Container style={{ marginTop: '7em' }}>
         <ActivityDashboard
-          activities={activityStore.activities}
+          activities={activities}
           selectActivity={handleSelectActivity}
           selectedActivity={selectedActivity}
           editMode={editMode}
@@ -109,4 +108,4 @@ const App = () => {
   );
 };
 
-export default observer(App);
+export default App;
